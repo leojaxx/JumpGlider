4cf9a68dd36a5f3d9206563a115812fd177836d0
Leonard Jackson <hello@leonardjackson.me>
2016-10-25 17:57:30 -0600

    fixed server crash

--- src/main/java/me/leonardjackson/jumpglider/event/EventHandlerCommon.java
+++ src/main/java/me/leonardjackson/jumpglider/event/EventHandlerCommon.java
@@ -1,8 +1,8 @@
 package me.leonardjackson.jumpglider.event;
 
-import me.leonardjackson.jumpglider.init.ModItems;
 import me.leonardjackson.jumpglider.items.ItemJumpGliderArmor;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,18 +14,20 @@ public class EventHandlerCommon {
 		if (event.getEntityLiving() instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer)event.getEntityLiving();
 			
-			if (!player.getEntityWorld().isRemote) {
-				if (player.inventory.armorItemInSlot(0) != null && player.inventory.armorItemInSlot(0).getItem() == ModItems.jumpGliderBoots) {
-					
-					double playerHeight = ItemJumpGliderArmor.letGo;
-					BlockPos block = player.getPosition();
-					double blockHeight = block.getY();
-					double distance = (playerHeight - blockHeight);
-					distance = distance - 4;
-					
-					if (player.posY - blockHeight >= distance)
-						event.setDistance((float)distance);
-				}
+			ItemStack itemStack = player.inventory.armorItemInSlot(0);
+			
+			if (itemStack != null && itemStack.getItem() instanceof ItemJumpGliderArmor) {
+				
+				ItemJumpGliderArmor armor = (ItemJumpGliderArmor)itemStack.getItem();
+				
+				double playerHeight = armor.letGo;
+				BlockPos block = player.getPosition();
+				double blockHeight = block.getY();
+				double distance = (playerHeight - blockHeight);
+				distance = distance - 4;
+				
+				if (player.posY - blockHeight >= distance)
+					event.setDistance((float)distance);
 			}
 		}
 	}
