4d4d23cc50ece4fc0d9f242ecbc2a45b8e91776d
Leonard Jackson <hello@leonardjackson.me>
2016-11-07 14:47:39 -0700

    update some info

--- src/main/java/me/leonardjackson/jumpglider/Reference.java
+++ src/main/java/me/leonardjackson/jumpglider/Reference.java
@@ -3,7 +3,7 @@ package me.leonardjackson.jumpglider;
 public class Reference {
 	public static final String MOD_ID = "jumpglider";
 	public static final String NAME = "Jump Glider";
-	public static final String VERSION = "A1.3";
+	public static final String VERSION = "A1.4";
 	public static final String ACCEPTED_VERSIONS = "1.10.2";
 
 	public static final String CLIENT_PROXY_CLASS = "me.leonardjackson.jumpglider.proxy.ClientProxy";
