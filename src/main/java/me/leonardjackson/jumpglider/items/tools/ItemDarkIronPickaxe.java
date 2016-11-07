bb1013d3ea48c922c39c29617801d6b019e8f434
Leonard Jackson <hello@leonardjackson.me>
2016-11-07 13:02:02 -0700

    added tool texures and model files

--- src/main/java/me/leonardjackson/jumpglider/items/tools/ItemDarkIronPickaxe.java
+++ src/main/java/me/leonardjackson/jumpglider/items/tools/ItemDarkIronPickaxe.java
@@ -9,8 +9,7 @@ public class ItemDarkIronPickaxe extends ItemPickaxe {
 	public ItemDarkIronPickaxe(ToolMaterial material) {
 		super(material);
 		setUnlocalizedName(Reference.JumpGliderItems.DARK_IRON_PICKAXE.getUnlocalizedName());
-		setRegistryName(Reference.JumpGliderItems.DARK_IRON_PICKAXE.getUnlocalizedName());
+		setRegistryName(Reference.JumpGliderItems.DARK_IRON_PICKAXE.getRegistryName());
 		setCreativeTab(JumpGlider.CREATIVE_TAB);
 	}
-
 }
